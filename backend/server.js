4404362e8bb7af4a07fa581b2200fa009aac2bc5
kappyllo <33810546+kappyllo@users.noreply.github.com>
2023-12-17 10:27:15 +0100

    working-on-put-req

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,14 @@ const taskModel = require("./schemas/Tasks");
 
 const uri = process.env.DATABASE_URL;
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 const app = express();
+app.use(cors(corsOptions));
 const port = 8080;
 
 async function connect() {
@@ -23,6 +30,21 @@ app.get("/", (req, res) => {
   res.send("TasksDb");
 });
 
+app.get("/getTasks", async (req, res) => {
+  try {
+    // res.set({
+    //   "Content-Type": "application/json",
+    //   "Access-Control-Allow-Origin": "*",
+    //   "Access-Control-Allow-Credentials": true,
+    // });
+    const tasks = await taskModel.find();
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("error??");
+  }
+});
+
 // app.get("/createTaskLol", (req, res) => {
 //   const testowyTaskMDb = new taskModel({
 //     id: Math.random(),
@@ -35,18 +57,18 @@ app.get("/", (req, res) => {
 //   res.send("dodano");
 // });
 
-app.get("/getTasks", async (req, res) => {
+app.put("/save", async (req, res) => {
   try {
-    res.set({
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
+    const resJS = res.body.json();
+    const response = new taskModel({
+      id: resJS.id,
+      name: resJS.name,
+      isDone: resJS.isDone,
     });
-    const tasks = await taskModel.find();
-    res.json(tasks);
+    response.save();
   } catch (err) {
     res.status(500).json({ message: err.message });
-    console.log("error??");
+    console.log("error");
   }
 });
 
